482d63844eb016097f15a4dce621c53dbcc3fdb7
anurag-2001 <anuragaecs1@gmail.com>
2021-06-30 21:40:21 +0530

    Objects

---
+++ tut2.js
@@ -0,0 +1,25 @@
+//OBJECTS
+
+let cat = {
+    name: "Sparky",
+    age: 12,
+    meow(){  //WE can also declare a function inside an OBJECT
+        alert("Meeooow");
+    }
+}
+
+cat.meow();  //This is the way how we should call a function inside an object
+
+//We can also add another object inside an existing object
+// ***EXAMPLE*** //
+
+let cat = {
+    name: "Sparky",
+    age: 12,
+    favFood:{
+        name1: "fish";
+        name2: "meat";
+    }
+}
+
+cat.favFood.name2;  //This is the way how we should another object inside existing object
