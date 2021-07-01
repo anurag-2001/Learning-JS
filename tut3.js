f6609b79bbead1798c70da70230be845339a995b
anurag-2001 <anuragaecs1@gmail.com>
2021-07-01 17:40:28 +0530

    Arrays

---
+++ tut3.js
@@ -0,0 +1,10 @@
+//Arrays
+let myFavNumb = [30,72,17,87];
+let myColors = ["red","blue","green"];
+//Adding an object inside the array
+let myPets = [{name:"Spark", speices:"Cat"}, {name:"Sparky",species:"Dog"}];
+console.log(myPets[1].species);
+
+myColors.push("Yellow"); //Yellow will be added inside the array myColors
+
+myColors.splice(0,1);  //Elemnt in 0th postion of an array would be deleted
