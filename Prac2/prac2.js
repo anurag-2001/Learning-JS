c8688effdef9b0bcd8b601fb7fbbe9729f9edee2
anurag-2001 <anuragaecs1@gmail.com>
2021-07-05 18:32:46 +0530

    ..

--- Prac2/prac2.js
+++ Prac2/prac2.js
@@ -3,17 +3,17 @@ let myInput = document.getElementById("myInput");
 let myList = document.getElementById("myList");
 
 myForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-    createItem(myInput.value);
+    e.preventDefault()     // As Submit button carries few inbuilt functions..we use preventDefault to reset its inbuilt function. 
+    createItem(myInput.value);     //Getting the value from our input box
 })
 
 function createItem(x){
     let ourHTML = `<li>${x} <button onclick = "deleteItem(this)">Delete</button> </li> <br>`
-    myList.insertAdjacentHTML("beforeend", ourHTML);
-    myInput.value = "";
-    myInput.focus();
+    myList.insertAdjacentHTML("beforeend", ourHTML);    
+    myInput.value = "";  //To reset the value after clicking on Submit
+    myInput.focus();    //To focus on the input even after submitting so that we can enter more elements in the list directly.  
 }
 
 function deleteItem(deleteMe){
-    deleteMe.parentElement.remove();
+    deleteMe.parentElement.remove();  //deletes the list inside the parent Element
 }
