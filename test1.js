845ceb4e46fbf194cb00ac6061aa39504a8215c4
anurag-2001 <anuragaecs1@gmail.com>
2021-07-06 01:04:08 +0530

    Created basic server using Node.js

---
+++ test1.js
@@ -0,0 +1,13 @@
+let http = require("http")          //We have to request the node.js... what we require and assign it to an variable 
+
+let ourApp = http.createServer(function(req, res){   //here we are creating a server with a functon having two parameters request"req" & response"res"
+    if(req.url == "/"){       //if local host has this ura show the message printed below
+        res.end("Welcome to my Home page")     //response.end and print the message"(....)"
+    }
+
+    if(req.url == "/about"){
+        res.end("Thanks for knowing about us")
+    }
+res.end("Sorry We can't find the page you are looking for")
+})
+ourApp.listen(3000)   //listen to the variable in which we have created the server with the port number "3000"
