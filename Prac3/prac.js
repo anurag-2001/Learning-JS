bf50ef050b9b616139d2fb95ee01a1150eb2b67e
anurag-2001 <anuragaecs1@gmail.com>
2021-07-15 02:33:16 +0530

    Tic Tac Toe

--- Prac3/prac.js
+++ Prac3/prac.js
@@ -3,7 +3,9 @@ for(i=0; i<cell.length; i++) {
     cell[i].addEventListener("click", mygame)
 }
 let dec = true
+let count = 0
 function mygame(){
+    count = count + 1;
     let inshort = event.target.textContent
     if(inshort == " " && dec == true){
         event.target.textContent  = "X"
@@ -13,4 +15,18 @@ function mygame(){
         event.target.textContent  = "O"
         dec = true
     } 
+    if(cell[0].textContent == "X" && cell[1].textContent == "X" && cell[2].textContent == "X" ||cell[3].textContent == "X" &&cell[4].textContent == "X" && cell[5].textContent == "X" ||cell[6].textContent == "X" && cell[7].textContent == "X" && cell[8].textContent == "X" || cell[0].textContent == "X" && cell[3].textContent == "X" && cell[6].textContent == "X" || cell[1].textContent == "X" && cell[4].textContent == "X" && cell[7].textContent == "X" || cell[2].textContent == "X" && cell[5].textContent == "X" && cell[8].textContent == "X" || cell[0].textContent == "X" && cell[4].textContent == "X" && cell[8].textContent == "X" || cell[2].textContent == "X" && cell[4].textContent == "X" && cell[6].textContent == "X")
+    {
+        let result = document.getElementById("result")
+        result.innerHTML = "Player 1 WON"
+    } 
+    else if(cell[0].textContent == "O" && cell[1].textContent == "O" && cell[2].textContent == "O" ||cell[3].textContent == "O" &&cell[4].textContent == "O" && cell[5].textContent == "O" ||cell[6].textContent == "O" && cell[7].textContent == "O" && cell[8].textContent == "O" || cell[0].textContent == "O" && cell[3].textContent == "O" && cell[6].textContent == "O" || cell[1].textContent == "O" && cell[4].textContent == "O" && cell[7].textContent == "O" || cell[2].textContent == "O" && cell[5].textContent == "O" && cell[8].textContent == "O" || cell[0].textContent == "O" && cell[4].textContent == "O" && cell[8].textContent == "O" || cell[2].textContent == "O" && cell[4].textContent == "O" && cell[6].textContent == "O")
+    {
+        let result = document.getElementById("result")
+        result.innerHTML = "Player 2 WON"
+    }
+    else if(count == 9){
+        let result = document.getElementById("result")
+        result.innerHTML = "DRAW"
+    }
 }
