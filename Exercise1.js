8e5361274e68ac93bce5f52f3b39931d1d44345b
anurag-2001 <anuragaecs1@gmail.com>
2021-08-05 22:15:15 +0530

    EX-1

--- Exercise1.js
+++ Exercise1.js
@@ -1,8 +1,6 @@
 //Accept the User name via Prompt and Greet by changing the first letter in Capital rest of the word should be small.
 
 let myName = prompt("Please enter your name");
-let firstLetter = myName.slice(0,1);
-firstLetter.toUpperCase();
-let rest = myName.slice(1,myName.length);
-rest.toLowerCase();
-alert("Welcome "+ firstLetter +" " + rest);
+let firstLetter = myName.slice(0,1);     //selecting the first letter by SLICE
+let rest = myName.slice(1,myName.length);  
+alert("Welcome "+ firstLetter.toUpperCase() + rest.toLowerCase());
